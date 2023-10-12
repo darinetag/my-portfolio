81c10f5067ca7a692635cd11f767c4bdd4cab194
Darine <tagdarinefatma@gmail.com>
2023-10-12 16:37:10 +0100

    email section working

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -2,35 +2,9 @@
 import React from "react";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import Link from "next/link";
-import { useState } from "react";
+
 
 const EmailSection = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
-  };
   return (
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#649173] via-teal-900 to-blue-900 rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2"></div>
@@ -54,7 +28,12 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col" onSubmit={handleSubmit}>
+        <form
+          className="flex flex-col"
+          action="mailto:tagdarinefatma@gmail.com"
+          method="post"
+          encType="text/plain"
+        >
           <div className="mb-6">
             <label
               htmlFor="email"
@@ -110,11 +89,6 @@ const EmailSection = () => {
             {" "}
             Send message{" "}
           </button>
-          {emailSubmitted && (
-            <p className="text-green-500 text-sm mt-2">
-              Email sent successfully
-            </p>
-          )}
         </form>
       </div>
     </section>
--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 const NavLink = ({ href, title }) => {
   return (
     <Link
-      href={"href"}
+      href={href}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
       {title}
