d06a3bbf743cea51c707255e99ffa3945f816e18
Darine <tagdarinefatma@gmail.com>
2024-02-23 18:29:01 +0100

    add projects

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -6,6 +6,16 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
+    title: "ChatGPT Clone",
+    description:
+      "ChatGPT Clone, a powerful chat application using useSWR for data fetching and NextAuth for Firebase authentication",
+    tags: "#TypeScript #Next.js #OpenAI_API #TailwindCSS #Firebase #Firestore",
+    image: "/images/chatgpt.png",
+    gitUrl: "https://github.com/darinetag/chatgpt-clone",
+    previewUrl: "https://chatgpt-delta-vert.vercel.app/",
+  },
+  {
+    id: 2,
     title: "Shareity",
     description:
       "Shareity encourages people to share and reuse products like clothes, food and furniture amongst the community",
@@ -15,7 +25,7 @@ const projectsData = [
     previewUrl: "https://letshare-iota.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Portfolio",
     description:
       "My Portfolio showcases my projects, and all the necessary information concerning my skils, competencies, and how to contact me.",
@@ -25,7 +35,7 @@ const projectsData = [
     previewUrl: "https://darinetag.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "ChromX",
     description:
       "ChromX is an E-Commerce project designed to showcase the usage of various technologies for building a modern website.",
@@ -35,7 +45,17 @@ const projectsData = [
     previewUrl: "https://e-commerce-project-chromx.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
+    title: "ToDo App",
+    description:
+      "A simple Todo app built using React and Vite. It allows users to manage their tasks efficiently with a clean and intuitive user interface.",
+    tags: "#React.js #Vite",
+    image: "/images/ToDo.png",
+    gitUrl: "https://github.com/darinetag/Todo-react",
+    previewUrl: "https://github.com/darinetag/Todo-react",
+  },
+  {
+    id: 6,
     title: "Cosmic Nova",
     description:
       "Cosmic Nova is a madlibs game created with vanilla Javascript, CSS and HTML. It is responsive and the styling was inspired from Figma.",
